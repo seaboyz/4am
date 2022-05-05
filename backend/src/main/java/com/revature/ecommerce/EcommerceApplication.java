2fc2de2a37a7cd7e329012f207b53ef5ec199468
seaboyz <seaboyz@hotmail.com>
2022-05-04 21:06:03 -0500

    Test database connection

--- backend/src/main/java/com/revature/ecommerce/EcommerceApplication.java
+++ backend/src/main/java/com/revature/ecommerce/EcommerceApplication.java
@@ -1,5 +1,8 @@
 package com.revature.ecommerce;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,21 @@ public class EcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+
+		// configure the database
+		Configuration config = new Configuration();
+		config.configure("hibernate.cfg.xml");
+
+		// build the session factory
+		SessionFactory sessionFactory = config.buildSessionFactory();
+		// get the session
+		Session session = sessionFactory.openSession();
+		// start a transaction
+		session.beginTransaction();
+		System.out.println(session.isConnected());
+		// close the session
+		session.close();
+		System.out.println(session.isConnected());
 	}
 
 }
