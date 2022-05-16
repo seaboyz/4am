61c44de852a2e34d1f7a477288a91ce8d04e454f
seaboyz <seaboyz@hotmail.com>
2022-05-16 06:34:17 -0500

    Trying to save customer from spring boot with hibernate

--- backend/src/main/java/com/webdev/controller/CustomerController.java
+++ backend/src/main/java/com/webdev/controller/CustomerController.java
@@ -1,5 +1,10 @@
 package com.webdev.controller;
 
+import com.webdev.dao.CustomerDao;
+import com.webdev.model.Customer;
+import com.webdev.utils.TestUtil;
+
+import org.hibernate.SessionFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @GetMapping("/customers")
     public String getCustomers() {
-        return "customers";
+        SessionFactory sessionFactory = TestUtil.getSessionFactory();
+
+        CustomerDao customerDao = new CustomerDao(sessionFactory);
+
+        Customer customer = new Customer(
+                "johnd",
+                "john@gmail.com",
+                "m38rmF$",
+                "1-570-236-7033");
+
+        customerDao.add(customer);
+
+        return customer.toString();
+
     }
 }
