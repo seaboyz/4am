9ffc37f8572cc149d9830da5c99a4155ed3e43d7
seaboyz <seaboyz@hotmail.com>
2022-05-16 06:53:46 -0500

    todo: figuring out how to connection spring-boot to hibernate

--- backend/src/main/java/com/webdev/controller/CustomerController.java
+++ backend/src/main/java/com/webdev/controller/CustomerController.java
@@ -8,21 +8,17 @@ import org.hibernate.SessionFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// todo: fix connection between CustomerController and CustomerDao
 @RestController
 public class CustomerController {
     @GetMapping("/customers")
     public String getCustomers() {
-        SessionFactory sessionFactory = TestUtil.getSessionFactory();
-
-        CustomerDao customerDao = new CustomerDao(sessionFactory);
 
         Customer customer = new Customer(
                 "johnd",
                 "john@gmail.com",
                 "m38rmF$",
                 "1-570-236-7033");
-
-        customerDao.add(customer);
 
         return customer.toString();
 
