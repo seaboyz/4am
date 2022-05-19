5246a288de3aabe53161627b4def423a1242efcf
seaboyz <seaboyz@hotmail.com>
2022-05-19 09:48:32 -0500

    merge testing

--- backend/src/test/java/com/webdev/dao/CustomerDaoTest.java
+++ backend/src/test/java/com/webdev/dao/CustomerDaoTest.java
@@ -1,7 +1,6 @@
 package com.webdev.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import javax.persistence.EntityManager;
 
