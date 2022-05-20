0811a988967c7a172d697685a6cf3b4176ba3961
seaboyz <seaboyz@hotmail.com>
2022-05-20 09:51:25 -0500

    Add AuthService test

---
+++ backend/src/test/java/com/webdev/service/AuthSeriviceTest.java
@@ -0,0 +1,15 @@
+package com.webdev.service;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthSeriviceTest {
+    @Test
+    void testLogin() {
+
+    }
+
+    @Test
+    void testRegister() {
+
+    }
+}
