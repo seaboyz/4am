4144e8a1d40de3e221d4b810a68ead3b86a0bb22
seaboyz <seaboyz@hotmail.com>
2022-05-20 10:39:01 -0500

    update orderServiceTest

--- backend/src/test/java/com/webdev/service/OrderServiceTest.java
+++ backend/src/test/java/com/webdev/service/OrderServiceTest.java
@@ -1,105 +1,100 @@
 package com.webdev.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.EntityManager;
-
+import com.webdev.dao.OrderDao;
 import com.webdev.model.Customer;
+import com.webdev.model.Order;
 import com.webdev.model.OrderItem;
 import com.webdev.model.Product;
 import com.webdev.model.ShippingAddress;
 
-import org.hibernate.Session;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-// integration test for OrderService
-
-@Disabled
+@ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
 
-        @Autowired
-        EntityManager entityManager;
+	@Mock
+	private OrderDao orderDao;
 
-        @Autowired
-        OrderService orderService;
+	@Mock
+	private CustomerService customerService;
 
-        @Autowired
-        CustomerService customerService;
+	@Mock
+	private ProductService productService;
 
-        Session currentSession;
+	private OrderService orderService;
 
-        @Disabled
-        @Test
-        public void testPlaceAOrder() {
+	@BeforeEach
+	public void setUp() {
+		orderService = new OrderService(orderDao, customerService, productService);
+	}
 
-                Product product1 = new Product(
-                                "Fjallraven - Foldsack No. 1Backpack, Fits 15 Laptops",
-                                "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-                                109.95,
-                                "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-                                "men's clothing");
+	@Disabled
+	@Test
+	public void testPlaceAOrder() {
 
-                Product product2 = new Product(
-                                "Mens Casual Premium Slim Fit T-Shirts",
-                                "Slim-fitting style, contrast raglan long sleeve, three-button henley placket",
-                                22.3,
-                                "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-                                "men's clothing");
+		Product product1 = new Product(
+				"Fjallraven - Foldsack No. 1Backpack, Fits 15 Laptops",
+				"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+				109.95,
+				"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+				"men's clothing");
 
-                ShippingAddress shippingAddress = new ShippingAddress(
-                                "david",
-                                "morrison",
-                                "86 Frances Ct",
-                                "",
-                                "Cullman",
-                                "TN",
-                                "38301",
-                                "USA");
+		Product product2 = new Product(
+				"Mens Casual Premium Slim Fit T-Shirts",
+				"Slim-fitting style, contrast raglan long sleeve, three-button henley placket",
+				22.3,
+				"https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+				"men's clothing");
 
-                Customer customer = new Customer("johnd", "john@gmail.com", "m38rmF",
-                "555-555-5555");
+		ShippingAddress shippingAddress = new ShippingAddress(
+				"david",
+				"morrison",
+				"86 Frances Ct",
+				"",
+				"Cullman",
+				"TN",
+				"38301",
+				"USA");
 
-                OrderItem orderItem1 = new OrderItem(product1, 1);
-                OrderItem orderItem2 = new OrderItem(product2, 2);
+		Customer customer = new Customer("johnd", "john@gmail.com", "m38rmF",
+				"555-555-5555");
 
-                Set<OrderItem> orderItems = new HashSet<OrderItem>();
-                orderItems.add(orderItem1);
-                orderItems.add(orderItem2);
+		OrderItem orderItem1 = new OrderItem(product1, 1);
+		OrderItem orderItem2 = new OrderItem(product2, 2);
 
-                currentSession = entityManager.unwrap(Session.class);
-                currentSession.beginTransaction();
-                currentSession.save(product1);
-                currentSession.save(product2);
-                currentSession.save(customer);
-                Integer customerId = customer.getId();
-                currentSession.getTransaction().commit();
-                currentSession.close();
+		Set<OrderItem> orderItemList = new HashSet<OrderItem>();
+		orderItemList.add(orderItem1);
+		orderItemList.add(orderItem2);
 
-                System.out.println(customerId);
+		Order order = new Order(customer, shippingAddress, orderItemList);
 
-                // place a order
+		when(customerService.getCustomerById(1)).thenReturn(customer);
+		when(productService.getProductById(1)).thenReturn(product1);
+		when(productService.getProductById(2)).thenReturn(product2);
 
-                HashMap<Integer, Integer> orderList = new HashMap<Integer, Integer>();
-                orderList.put(product1.getId(), 1);
-                orderList.put(product2.getId(), 2);
+		when(orderDao.add(order)).thenReturn(1);
 
-                Integer orderId = orderService.placeAOrder(
-                                customerId,
-                                shippingAddress,
-                                orderList);
+		when(customerService.addAddressToCustomer(1, shippingAddress)).thenReturn(customer);
 
-                customer = customerService.getCustomerById(customerId);
+		HashMap<Integer, Integer> itemList = new HashMap<>();
+		itemList.put(1, 1);
+		itemList.put(2, 2);
 
-                assertEquals(1, orderId);
+		Integer orderId = orderService.placeAOrder(1, shippingAddress, itemList);
 
-                assertEquals(1, customer.getOrders().size());
+		assert orderId == 1;
 
-        }
+	}
 
 }
